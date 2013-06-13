69fea214b2d53cf6fd39cb21e0c448e056c8555a
Daniele Giove <DanieleGiove@JDN-IMAC.local>
2013-06-13 17:19:06 +0200

    dg

--- Zesser/JDNCurrentWeatherView.m
+++ Zesser/JDNCurrentWeatherView.m
@@ -13,11 +13,12 @@
 - (id)initWithFrame:(CGRect)frame{
     self = [super initWithFrame:frame];
     if (self) {
-        self.temperature = [[UILabel alloc] initWithFrame:CGRectMake(10, 25, 50, 32)];
+        self.temperature = [[UILabel alloc] initWithFrame:CGRectMake(10, 28, 50, 32)];
         self.temperature.textAlignment = NSTextAlignmentCenter;
-        self.forecastImage = [[UIImageView alloc] initWithFrame:CGRectMake(70, 25, 32, 32)];
         self.temperature.font = [UIFont fontWithName:@"Helvetica-Bold" size:25];
         self.temperature.backgroundColor = [UIColor clearColor];
+        
+        self.forecastImage = [[UIImageView alloc] initWithFrame:CGRectMake(70, 28, 32, 32)];
         self.forecastImage.backgroundColor = [UIColor clearColor];
         
         [self addSubview:self.temperature];
--- Zesser/JDNSummaryWeatherView.m
+++ Zesser/JDNSummaryWeatherView.m
@@ -15,11 +15,11 @@
     self = [super initWithFrame:frame];
     if (self) {
 
-        self.forecast = [[UILabel alloc] initWithFrame:CGRectMake(0, 0, frame.size.width, 19)];
+        self.forecast = [[UILabel alloc] initWithFrame:CGRectMake(0, 2, frame.size.width, 19)];
         self.forecast.font = [UIFont systemFontOfSize:9.0];
         self.forecast.textColor = [UIColor colorWithRed:0.878 green:0.673 blue:0.926 alpha:1.000];
         
-        self.wind = [[UILabel alloc] initWithFrame:CGRectMake(0, 20, frame.size.width, 25)];
+        self.wind = [[UILabel alloc] initWithFrame:CGRectMake(0, 22, frame.size.width, 25)];
         self.wind.font = [UIFont systemFontOfSize:11.0];
         self.wind.textColor = [UIColor colorWithRed:0.645 green:0.901 blue:0.916 alpha:1.000];
         self.wind.lineBreakMode = NSLineBreakByTruncatingTail;
